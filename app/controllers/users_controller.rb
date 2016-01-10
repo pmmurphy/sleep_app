d1a54bf7e239fe062d92ef3348c57ae07c3e8810
Paula Murphy <paulam849@gmail.com>
2016-01-10 15:43:20 -0500

    added form for

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,23 @@
 class UsersController < ApplicationController
   def index
+  	@users = User.all
   end
+
+  def new
+  	@user = User.new 
+  end
+
+  def create
+  	@user = User.new(params[:user])
+  	if @user.save
+		flash[:notice] = "Your account was created
+		successfully."
+	# redirect_to user_path @user
+	redirect_to root_path
+	else
+		flash[:alert] = "There was a problem saving your
+		account."
+		redirect_to new_user_path
+   end
+ end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,17 @@
 Rails.application.routes.draw do
   
 
-  get 'users/index'
+  # get 'users/index'
+  get "signup", to: "users#new"
 
+  resources :users
+  
+  # get this routs using this action
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'welcome#index'
+  root 'welcome#home'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -16,7 +20,7 @@ Rails.application.routes.draw do
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
+    # resources :users
 
   # Example resource route with options:
   #   resources :products do
